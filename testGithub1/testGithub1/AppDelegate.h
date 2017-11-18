81e494a63bd50e40a3212da4b103739079dfad52
TiuBen <shentiuben@gmail.com>
2017-11-18 10:34:39 +0800

    Xcode defaut files

---
+++ testGithub1/testGithub1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  testGithub1
+//
+//  Created by 沈宁 on 11/18/17.
+//  Copyright © 2017 沈宁. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ testGithub1/testGithub1/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  testGithub1
+//
+//  Created by 沈宁 on 11/18/17.
+//  Copyright © 2017 沈宁. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
