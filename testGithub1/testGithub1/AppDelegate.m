b399457ae9c3fd6f7ec540ddee5e39ad6ac390d0
TiuBen <shentiuben@gmail.com>
2017-11-18 13:10:06 +0800

    Add WindowController

--- testGithub1/testGithub1/AppDelegate.m
+++ testGithub1/testGithub1/AppDelegate.m
@@ -10,13 +10,23 @@
 
 @interface AppDelegate ()
 
-@property (weak) IBOutlet NSWindow *window;
+
 @end
 
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     // Insert code here to initialize your application
+    NSWindow *firstWindow=[[NSWindow alloc] initWithContentRect:NSMakeRect(0, 0, 400, 300)
+                                                     styleMask:NSTitledWindowMask|NSResizableWindowMask
+                                                       backing:NSBackingStoreBuffered
+                                                         defer:YES];
+   
+    [firstWindow setTitle:@"first Window"];
+    MainWindowController *mainWC=[[MainWindowController alloc] initWithWindow:firstWindow];
+    _mainWindow=mainWC;
+    [_mainWindow.window makeKeyAndOrderFront:nil];
+    
 }
 
 
---
+++ testGithub1/testGithub1/View/MainWindowController.h
@@ -0,0 +1,13 @@
+//
+//  MainWindowController.h
+//  testGithub1
+//
+//  Created by 沈宁 on 11/18/17.
+//  Copyright © 2017 沈宁. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MainWindowController : NSWindowController
+
+@end
